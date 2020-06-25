20307df28f92fb3a08d4956ba3f273cb75502e4a
repl.it user <replituser@example.com>
2020-06-25 06:45:39 +0000

    implementation of second stage

--- Main.java
+++ Main.java
@@ -1,16 +1,37 @@
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        char[][] board = {
-            {'X', 'O', 'X'},
-            {'O', 'X', 'O'},
-            {'X', 'X', 'O'},
-        };
+    private static char[][] board = new char[3][3];
+    private static Scanner scan = new Scanner(System.in);
+
+    private static void printBoard() {
+        System.out.println("---------");
+        
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("%c ", board[i][j]);
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
+    }
+    
+    private static void getInput() {
+        System.out.println("Enter cells: ");
+        String s = scan.nextLine();
+
+        int cur = 0;
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
+                board[i][j] = s.charAt(cur++);
             }
-            System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        getInput();
+        printBoard();
     }
 }
