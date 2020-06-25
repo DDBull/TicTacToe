8027994ae563598ed4b2c61f8c8f62ccddfd2bf7
repl.it user <replituser@example.com>
2020-06-25 06:27:22 +0000

    implementation of first stage

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        
+        char[][] board = {
+            {'X', 'O', 'X'},
+            {'O', 'X', 'O'},
+            {'X', 'X', 'O'},
+        };
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
